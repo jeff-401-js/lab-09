b06c5ad6801440cbf54b5774279ff5fe1f76b16b
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-06 19:36:21 -0700

    jsdoc middleware work

--- src/middleware/500.js
+++ src/middleware/500.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+ * @module src/middleware/500
+ */
+
+/**
+   * @param {object} req - request object
+   * @param {object} res - response object
+   * @desc server error handler
+   * Export object
+   * @type {Object}
+   */
 
 module.exports = (err, req, res, next) => {
   let error = { error: err };
